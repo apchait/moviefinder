3ab30be7692f4c1d0036fae007c4a47f668e6bfe
Ariel Chait <apchait@gmail.com>
2014-03-06 00:47:56 -0800

    routes updated for map view at root

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ UberHw::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'map#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
